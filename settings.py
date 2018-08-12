2e1c7828f9d007c0ea753e79ac45e4c8d9f31c20
samlazrak <samlazrak@outlook.com>
2018-08-12 12:08:25 -0500

    inital pygame commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+light_green = (50, 205, 50)
+forest_green = (34, 139, 34)
+green = (0, 128, 0)
+red = (255, 0, 0)
+brown = (128, 128, 0)
