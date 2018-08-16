e59b3d902c7700ded605ac93cfc34ed87f6933c9
samlazrak <samlazrak@outlook.com>
2018-08-15 23:05:13 -0500

    added numpy and matplot lib as new method for grid

--- Simulate.py
+++ Simulate.py
@@ -1,10 +1,14 @@
-from Objects.Ecology import Ecology
+import numpy as np
+import matplotlib.pyplot as plt
 
-Ecology = Ecology()
+universe = np.zeros((6, 6))
+
+beacon = [[1, 1, 0, 0],
+          [1, 1, 1, 0],
+          [0, 0, 1, 1],
+          [0, 0, 1, 1]]
+universe[1:5, 1:5] = beacon
 
 
-def test():
-  print('test')
-
-
-test()
+plt.imshow(universe, cmap='binary')
+plt.show()
